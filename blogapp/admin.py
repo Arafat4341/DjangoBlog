0efa555f7f998f1cbe94694b89dea7a54c359f57
Arafat4341 <mdyasinarafatyen2@gmail.com>
2019-01-09 12:08:07 -0800

    admin section customization

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
 from .models import Author, Category, Article
 
-admin.site.register(Author)
-admin.site.register(Category)
-admin.site.register(Article)
+
+class authorModel(admin.ModelAdmin):
+	list_display = ['__str__']
+	search_fields = ['__str__', 'details']
+	class Meta:
+		Model = Author
+		
+admin.site.register(Author, authorModel)
+
+class articleModel(admin.ModelAdmin):
+	list_display = ['__str__', 'posted_on']
+	search_fields = ['__str__', 'title']
+	list_filter = ['posted_on', 'category']
+	list_per_page = 10
+	class Meta:
+		Model = Article
+admin.site.register(Article, articleModel)
+
+class categoryModel(admin.ModelAdmin):
+	list_display = ['__str__']
+	search_fields = ['__str__']
+	class Meta:
+		Model = Category
+
+admin.site.register(Category, categoryModel)
+
+
