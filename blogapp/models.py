ea846f4246c1dbe986f54efad5793f50faa1fc6b
Arafat4341 <mdyasinarafatyen2@gmail.com>
2019-01-09 11:09:29 -0800

    models added

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Author(models.Model):
+	name = models.ForeignKey(User, on_delete=models.CASCADE)
+	details = models.TextField()
+
+	def __str__(self):
+		return self.name.username
+
+
+class Category(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
+
+class Article(models.Model):
+	article_author = models.ForeignKey(Author, on_delete=models.CASCADE)
+	title = models.CharField(max_length=200)
+	body = models.TextField()
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
+		
